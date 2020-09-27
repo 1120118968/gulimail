34f65a916d453dbe568f16cfbcbd7a72c83e8256
Administrator <1150118968@qq.com>
2020-09-27 13:34:02 +0800

    Initial commit
    Signed-off-by: Administrator <1150118968@qq.com>

---
+++ gulimail-product/src/test/java/com/example/gulimail/gilimailproduct/GilimailProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gulimail.gilimailproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GilimailProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
