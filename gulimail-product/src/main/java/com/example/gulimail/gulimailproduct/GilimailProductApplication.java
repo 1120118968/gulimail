75ccfe064d7204e94d189584ae5d1c9a59a81a0a
Administrator <1150118968@qq.com>
2020-09-27 14:40:19 +0800

    Initial commit
    Signed-off-by: Administrator <1150118968@qq.com>

--- gulimail-product/src/main/java/com/example/gulimail/gulimailproduct/GilimailProductApplication.java
+++ gulimail-product/src/main/java/com/example/gulimail/gulimailproduct/GilimailProductApplication.java
@@ -1,4 +1,4 @@
-package com.example.gulimail.gilimailproduct;
+package com.example.gulimail.gulimailproduct;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- gulimail-product/src/test/java/com/example/gulimail/gulimailproduct/GilimailProductApplicationTests.java
+++ gulimail-product/src/test/java/com/example/gulimail/gulimailproduct/GilimailProductApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.gulimail.gilimailproduct;
+package com.example.gulimail.gulimailproduct;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
